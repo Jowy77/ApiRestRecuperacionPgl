46aed2454a9b82b08e51b6d79647cc40faa1dbf7
jowy77 <iproyectz@hotmail.com>
2024-04-23 11:19:49 +0200

    Initial commit

---
+++ src/test/java/com/example/apirestrecuperacionpgl/ApiRestRecuperacionPglApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apirestrecuperacionpgl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestRecuperacionPglApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
